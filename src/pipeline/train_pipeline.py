3b39bee7a944cbbe26415033b1d9d3c96cc8961e
shaileshgaddam22@gmail.com <shaileshgaddam22@gmail.com>
2023-07-20 19:40:50 +0530

    Training Pipeline Completed

---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+sys.path.append(r"D:\Practice Machine Learning\Practice-Project")
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data , test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr , test_arr , _ = data_transformation.initaite_data_transformation(train_data , test_data)
+
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr , test_arr)
